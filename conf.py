2d2fc51def312b4417aa75b33ec9285a91799ae5
Assanali Mukhanov <22858781+shivaco@users.noreply.github.com>
2020-10-22 14:46:16 +0600

    Add files via upload

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -23,27 +23,29 @@ author = 'Epic'
 
 # -- General configuration ---------------------------------------------------
 
+html_experimental_html5_writer = True
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinxcontrib_trio'
 ]
 
-# autodoc_member_order = 'bysource'
+autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
-master_doc = 'index'
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'friendly'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -51,20 +53,24 @@ master_doc = 'index'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {'show_powered_by': False}
+# html_theme_options = {
+#     'sidebar_width': "240px",
+#     'page_width'   : "1200px",
+#     'show_powered_by': False
+# }
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-html_show_sphinx = False
+# html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = True
+# html_show_copyright = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'basic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
